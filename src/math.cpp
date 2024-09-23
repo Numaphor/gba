03534ea07e5c2ae06dd23f1cd3fd1dd6f15599ab
lazy-demon <jefdayt@gmail.com>
2024-09-23 23:33:05 +0200

    chore: Add 'format' library to include list in grid.h

--- src/math.cpp
+++ src/math.cpp
@@ -1,53 +1,43 @@
-#include <bn_array.h>
-#include <bn_vector.h>
-#include <bn_fixed_point.h>
-#include <bn_core.h>
 #include "math.h"
+#include <bn_array.h>
+#include <bn_core.h>
+#include <bn_fixed_point.h>
+#include <bn_vector.h>
 
 namespace dnd
 {
-    bn::fixed_point math::isoToCart(bn::fixed_point pos)
-    {
-        bn::fixed_point cart;
+bn::fixed_point math::isoToCart(bn::fixed_point pos)
+{
+    bn::fixed_point cart;
 
-        cart.set_x(((pos.x() * dnd::config::TILE_WIDTH) / 2) - ((pos.y() * dnd::config::TILE_WIDTH) / 2));
-        cart.set_y(((pos.x() * dnd::config::TILE_HEIGHT) / 4) + ((pos.y() * dnd::config::TILE_HEIGHT) / 4));
+    cart.set_x(((pos.x() * dnd::config::TILE_WIDTH) / 2) - ((pos.y() * dnd::config::TILE_WIDTH) / 2));
+    cart.set_y(((pos.x() * dnd::config::TILE_HEIGHT) / 4) + ((pos.y() * dnd::config::TILE_HEIGHT) / 4));
 
-        return cart;
-    }
+    return cart;
+}
 
-    bn::fixed_point math::cartToIso(bn::fixed_point pos)
-    {
-        bn::fixed_point iso;
-        pos.set_y(pos.y());
+bn::fixed_point math::cartToIso(bn::fixed_point pos)
+{
+    bn::fixed_point iso;
+    pos.set_y(pos.y());
 
-        iso.set_x((pos.x() / (dnd::config::TILE_WIDTH / 2) + pos.y() / (dnd::config::TILE_HEIGHT / 4)) / 2);
-        iso.set_y((pos.y() / (dnd::config::TILE_HEIGHT / 4) - pos.x() / (dnd::config::TILE_WIDTH / 2)) / 2);
+    iso.set_x((pos.x() / (dnd::config::TILE_WIDTH / 2) + pos.y() / (dnd::config::TILE_HEIGHT / 4)) / 2);
+    iso.set_y((pos.y() / (dnd::config::TILE_HEIGHT / 4) - pos.x() / (dnd::config::TILE_WIDTH / 2)) / 2);
 
-        return iso;
-    }
-    // make functions that take the pos + how many tiles to move and return the new pos relative to the grid
+    return iso;
+}
+// functions that take the pos + how many tiles to move and return the new pos relative to the grid. the directions are
+// in cartesian coordinates and the position and destionation are in isometric coordinates
 
-    bn::fixed_point math::move(bn::fixed_point pos, bn::fixed_point loc)
-    {
-        //  cart to iso
-        bn::fixed_point iso;
+bn::fixed_point math::move(bn::fixed_point position, bn::fixed_point direction)
+{
+    bn::fixed_point isometricPosition;
+    isometricPosition = cartToIso(position);
+    isometricPosition.set_x(isometricPosition.x() + direction.x());
+    isometricPosition.set_y(isometricPosition.y() + direction.y());
+    bn::fixed_point destination = isoToCart(isometricPosition);
 
-        iso.set_x(isoToCart(pos).x());
-        iso.set_y(isoToCart(pos).y());
-
-        //  move
-
-        iso.set_x(iso.x() + loc.x());
-        iso.set_y(iso.y() + loc.y());
-
-        //  iso to cart
-        bn::fixed_point cart;
-
-        cart.set_x(cartToIso(iso).x());
-        cart.set_y(cartToIso(iso).y());
-
-        return cart;
-    }
+    return destination;
+}
 
 } // namespace dnd
--- src/world.cpp
+++ src/world.cpp
@@ -1,37 +1,28 @@
 #include <bn_array.h>
-#include <bn_vector.h>
-#include <bn_point.h>
 #include <bn_core.h>
+#include <bn_point.h>
+#include <bn_vector.h>
 
+#include "bn_keypad.h"
 #include "bn_sprite_items_world.h"
 #include "world.h"
-#include "bn_keypad.h"
 
 namespace dnd
 {
-    world::world(dnd::math &_math) : bg(bn::regular_bg_items::overworld.create_bg(0, 0)), player(bn::point(0, 0), bn::random(), _math), grid(_math), math_ref(_math)
-    {
-
-        // for (int y = 0; y < _math.getworldHeight(); y++)
-        // {
-        //     for (int x = 0; x < _math.getworldWidth(); x++)
-        //     {
-        //         bn::point cart = _math.isoToCart(bn::point(x, y));
-        //         dnd::Tile tile(bn::sprite_items::world.create_sprite(cart.x(), cart.y() - 1));
-        //         tile.getSprite().set_visible(false);
-        //         _math.getTiles().push_back(tile);
-        //     }
-        // }
-    }
-
-    void world::setBackground()
-    {
-        bg.set_priority(3);
-    }
-
-    void world::update()
-    {
-        player.update();
-        grid.update();
-    }
+world::world(dnd::math& _math)
+    : bg(bn::regular_bg_items::overworld.create_bg(0, 0)), player(bn::point(0, 0), bn::random(), _math), grid(_math),
+      math_ref(_math)
+{
 }
+
+void world::setBackground()
+{
+    bg.set_priority(3);
+}
+
+void world::update()
+{
+    // player.update();
+    grid.update();
+}
+} // namespace dnd
